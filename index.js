1c722b5063873d949d623a17eb4c9a357d8db3f7
Alin Dumitru <alin.dumitru20@gmail.com>
2020-01-24 15:51:07 +0200

    added dummy server with dummy data

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
-var express = require("express");
+var express = require('express');
 var app = express();
+const jsonServer = require('json-server');
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
 
-app.use(express.static(__dirname + "/public"));
+app.use(middlewares);
+app.use('/api', router);
 
-app.get("/", function(request, response) {
-  response.render("index.html");
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(request, response) {
+    response.render('index.html');
 });
 
 app.listen(3000, function() {
-  console.log("server is runing");
+    console.log('server is runing');
 });
