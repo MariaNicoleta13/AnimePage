c31302037931e687e7918d496f2a3dc335abeb41
Mary <maria.nicoleta.ion13@gmail.com>
2020-01-24 17:18:59 +0200

    navbar initial styling

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-var express = require('express');
+var express = require("express");
 var app = express();
-const jsonServer = require('json-server');
-const router = jsonServer.router('db.json');
+const jsonServer = require("json-server");
+const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
-
+//"/api/animes" link server
 app.use(middlewares);
-app.use('/api', router);
+app.use("/api", router);
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + "/public"));
 
-app.get('/', function(request, response) {
-    response.render('index.html');
+app.get("/", function(request, response) {
+  response.render("index.html");
 });
 
 app.listen(3000, function() {
-    console.log('server is runing');
+  console.log("server is running");
 });
