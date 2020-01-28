afafd2ab77da169d1017befa52184a63b9573524
Mary <maria.nicoleta.ion13@gmail.com>
2020-01-28 21:17:42 +0200

    aded text description and defined card box size

--- public/action.js
+++ public/action.js
@@ -17,20 +17,41 @@ function iterate(x) {
     if (index % 3 == 1) {
       list(element, "middle");
     } else if (index % 3 == 0) list(element, "first");
-    else if (index % 3==2)list(element,"third");
+    else if (index % 3 == 2) list(element, "third");
   });
 }
 function list(element, clas) {
   var listElement = document.createElement("LI"); //create <li>
-  listElement.setAttribute("class",clas);
-  
+  listElement.setAttribute("class", clas);
+
   listElement.classList.add("animeItem");
-  var divElement = document.createElement("DIV"); //create <div>
-  divElement.setAttribute("class","card");
+  var divElement = document.createElement("DIV"); //create <div class="card">
+  divElement.setAttribute("class", "card");
+
   listElement.appendChild(divElement);
+
+
   var imageElement = document.createElement("IMG"); //create <img>
   imageElement.setAttribute("src", element.image);
   imageElement.setAttribute("class", "animeImage");
   divElement.appendChild(imageElement);
   document.querySelector(".animeList").appendChild(listElement);
+
+  var divText = document.createElement("DIV"); //create <div class="description"> in card
+  divText.setAttribute("class", "description");
+  divElement.appendChild(divText);
+
+  var headerText=document.createElement("DIV");//<div class="headerText"> in description
+  divText.appendChild(headerText);
+  headerText.setAttribute("class","headerText");
+  headerText.appendChild(document.createTextNode(element.status));
+  headerText.appendChild(document.createTextNode(" "));
+  headerText.appendChild(document.createTextNode(element.details));
+
+
+
+  var pMainText = document.createElement("P"); //create <p>
+  divText.appendChild(pMainText);
+  pMainText.appendChild(document.createTextNode(element.description));
+  
 }
