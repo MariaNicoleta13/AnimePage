018bdb535c573cea683ba25417446b7758693b76
Mary <maria.nicoleta.ion13@gmail.com>
2020-02-04 20:58:49 +0200

    created createStatsContent function and styled the elements in it

--- public/action.js
+++ public/action.js
@@ -4,10 +4,12 @@ function reqListener() {
   // console.log(objData.length);
   iterate(objData);
 }
+
 var oReq = new XMLHttpRequest();
 oReq.addEventListener("load", reqListener);
 oReq.open("GET", "/api/animes");
 oReq.send();
+
 function iterate(x) {
   x.forEach(function(element, index) {
     // 0 1 2
@@ -20,6 +22,7 @@ function iterate(x) {
     else if (index % 3 == 2) list(element, "third");
   });
 }
+
 function list(element, clas) {
   var listElement = document.createElement("LI"); //create <li>
   listElement.setAttribute("class", clas);
@@ -43,44 +46,71 @@ function list(element, clas) {
   var headerText = document.createElement("DIV"); //<div class="headerText"> in description
   divText.appendChild(headerText);
   headerText.setAttribute("class", "header");
-  var divUpText=document.createElement("DIV");
+  var divUpText = document.createElement("DIV");
   headerText.appendChild(divUpText);
-  divUpText.setAttribute("class","headerText");
+  divUpText.setAttribute("class", "headerText");
 
-  element.status.forEach(function(oneStatus,index){
-    var pStatus=document.createElement("P");
+  element.status.forEach(function(oneStatus, index) {
+    var pStatus = document.createElement("P");
     divUpText.appendChild(pStatus);
     pStatus.appendChild(document.createTextNode(oneStatus));
-    var nr=index+1;
-    pStatus.setAttribute("class","status"+nr);
+    var nr = index + 1;
+    pStatus.setAttribute("class", "status" + nr);
   });
 
-  var pDetails=document.createElement("P");
+  var pDetails = document.createElement("P");
   divUpText.appendChild(pDetails);
   pDetails.appendChild(document.createTextNode(element.details));
 
-  var divStats=document.createElement("DIV");
-  headerText.appendChild(divStats);
-  divStats.setAttribute("class","stats");
-  var mood=document.createElement("IMG");
-  mood.setAttribute("class","mood");
-  mood.setAttribute("src","smile.png");
-  divStats.appendChild(mood);
+  createStatsContent(headerText);
 
   var pMainText = document.createElement("P"); //create <p>
   divText.appendChild(pMainText);
-  pMainText.setAttribute("class","mainText");
+  pMainText.setAttribute("class", "mainText");
   pMainText.appendChild(document.createTextNode(element.description));
 
   var divFooter = document.createElement("DIV"); //<div class="footer">
   divFooter.setAttribute("class", "footer");
   divText.appendChild(divFooter);
 
-  element.genres.forEach(function(genre) { //iterate to add one genre at once
+  element.genres.forEach(function(genre) {
+    //iterate to add one genre at once
     var pFooter = document.createElement("P"); //<div class="footer"><p>
     divFooter.appendChild(pFooter);
     pFooter.appendChild(document.createTextNode(genre));
     // console.log(genre);
   });
-  
+}
+
+function createStatsContent(headerText) {
+  var divStats = document.createElement("DIV"); //<div class="stats">
+  headerText.appendChild(divStats);
+  divStats.setAttribute("class", "stats");
+
+  var row1=document.createElement("DIV");
+  divStats.appendChild(row1);
+  row1.setAttribute("class","row1");
+
+  var mood = document.createElement("IMG");
+  mood.setAttribute("class", "mood");
+  mood.setAttribute("src", "smile.png");
+  row1.appendChild(mood);
+
+  var procent = document.createElement("P");
+  row1.appendChild(procent);
+  procent.appendChild(document.createTextNode("89%"));
+  procent.setAttribute("class", "likes");
+
+  var row2=document.createElement("DIV");
+  divStats.appendChild(row2);
+  row2.setAttribute("class","row2")
+
+  var loveIcon = document.createElement("IMG");
+  row2.appendChild(loveIcon);
+  loveIcon.setAttribute("src", "heart.png");
+
+  var loved=document.createElement("P");
+  loved.appendChild(document.createTextNode("#5"));
+  row2.appendChild(loved);
+
 }
